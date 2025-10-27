bd6fdc52a264372dc13f907bfd01316b2289bf43
nbebeshina <nadia.clairet@gmail.com>
2025-10-27 10:11:35 +0100

    Minor change

--- resources/templates.py
+++ resources/templates.py
@@ -2,12 +2,12 @@ summary= """Based on the following associations {associations}, write a short su
             Format the output as follows : {"summary": summary, "meta":{"gene": gene, "disease": disease}
             Do not provide comments. """
 
-response="""Based on the following description provide the list of gene symbols that may be related to the description. 
+response="""Based on the following description provide the list of gene symbols of up to 3 main genes that may be related to the description. 
             Description: {description}
             Response format: 
             - Gene 1: gene 1 symbol
             - Gene 2: gene 2 symbol
             - Gene 3:...
-            Do not give any explanation. No diagnosis. Only gene symbols.
+           Provide a brief explanation based on documents.
             
 """
